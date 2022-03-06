0c6e3fac98f4c237e0c6570f1e38c98c7acd06c7
Khurram Raza <ikhurramraza@gmail.com>
2022-03-06 12:32:15 +0100

    Bump inducker version from 0.1.0 to 0.2.0

--- inducker.rb
+++ inducker.rb
@@ -1,11 +1,24 @@
 class Inducker < Formula
   desc "Bootstraps running a docker image in current directory."
   homepage "https://github.com/ikhurramraza/inducker"
-  url "https://github.com/ikhurramraza/inducker/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1d1cfe2df39c9de17aa94642affcda77c17b8dfe622839898116f2f3e4a617c2"
+  url "https://github.com/ikhurramraza/inducker/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "726b9c7ae6d0233aaf2d12bf26573e619f23d7040b6a2f4c177a3f861d2b3c7d"
   license "MIT"
 
   def install
     bin.install "inducker"
+
+    install_templates!
+  end
+
+  private
+
+  TEMPLATES = %w[
+    inducker-ffmpeg
+    inducker-gallery-dl
+  ]
+
+  def install_templates!
+    TEMPLATES.each { |template| bin.install template }
   end
 end
